ff475c084659f05346440c60921663a19167e4e0
WHBoulware <misuses_mahouts_0d@icloud.com>
2026-02-17 05:16:27 -0500

    Update

---
+++ seerr/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bashio
+
+CONFIG_PATH="/config/seerr"
+
+# Create config folder if not present
+if [ ! -d "$CONFIG_PATH" ]; then
+    mkdir -p "$CONFIG_PATH"
+fi
+
+# Ensure correct permissions (node user = 1000)
+chown -R 1000:1000 "$CONFIG_PATH"
+
+export CONFIG_DIRECTORY="/config/seerr"
+export TZ=$(bashio::config 'TZ')
+export LOG_LEVEL=$(bashio::config 'LOG_LEVEL')
+
+exec /app/server.js
